883227706e8749978b15c578bd480aad76f88088
Your Name <ccsjgs1990@gmail.com>
2016-06-13 18:50:59 -0300

    controller.js parte libro

--- app/controller.js
+++ app/controller.js
@@ -1,5 +1,4 @@
 var Persona = require('./modelo/persona');
-
 // Obtiene todos los objetos Persona de la base de datos
 module.exports.getPersona = function (req, res){
 	Persona.find(
@@ -10,7 +9,6 @@ module.exports.getPersona = function (req, res){
 				}
 			);
 }
-
 // Guarda un objeto Persona en base de datos
 module.exports.setPersona = function(req, res) {
 
@@ -29,7 +27,6 @@ module.exports.setPersona = function(req, res) {
 			});
 
 	}
-
 // Modificamos un objeto Persona de la base de datos
 module.exports.updatePersona = function(req, res){
 	Persona.update( {_id : req.params.persona_id},
@@ -45,7 +42,6 @@ module.exports.updatePersona = function(req, res){
 				});
 			});
 	}
-
 // Elimino un objeto Persona de la base de Datos
 module.exports.removePersona = function(req, res) {
 	Persona.remove({_id : req.params.persona_id}, function(err, persona) {
@@ -56,6 +52,83 @@ module.exports.removePersona = function(req, res) {
 				if (err)
 					res.send(err)
 				res.json(persona);
+			});
+		});
+}
+
+
+//PARTE DE LIBRO
+var Libro = require('./modelo/libro');
+
+// Obtiene todos los objetos Libro de la base de datos
+module.exports.getLibro = function (req, res){
+	Libro.find(
+		function(err, libro) {
+			if (err)
+				res.send(err)
+					res.json(libro); // devuelve todas los Libros en JSON		
+				}
+			);
+}
+
+// Guarda un objeto Libro en base de datos
+module.exports.setLibro = function(req, res) {
+
+		// Creo el objeto Libro
+		Libro.create(
+			{nombre : req.body.nombre,
+				isbn: req.body.isbn, 
+				preciolocal: req.body.preciolocal,
+				autor: req.body.autor,
+				//portada: req.body.portada,
+				editorial: req.body.editorial,
+				ranking: req.body.ranking,
+				genero: req.body.genero,
+				paginas: req.body.paginas
+
+			}, 
+			function(err, libro) {
+				if (err)
+					res.send(err);
+				// Obtine y devuelve todos los libros tras crear uno de ellos
+				Libro.find(function(err, libro) {
+				 	if (err)
+				 		res.send(err)
+				 	res.json(libro);
+				});
+			});
+	}
+
+// Modificamos un objeto Libro de la base de datos
+module.exports.updateLibro = function(req, res){
+	Libro.update( {_id : req.params.libro_id},
+					{$set:{nombre : req.body.nombre,isbn: req.body.isbn, 
+				preciolocal: req.body.preciolocal,autor: req.body.autor,
+				//portada: req.body.portada,
+				editorial: req.body.editorial,ranking: req.body.ranking,
+				genero: req.body.genero,paginas: req.body.paginas
+
+					}}, 
+					function(err, libro) {
+						if (err)
+							res.send(err);
+				Libro.find(function(err, libro) {
+				 	if (err)
+				 		res.send(err)
+				 	res.json(libro);
+				});
+			});
+	}
+
+// Elimino un objeto Libro de la base de Datos
+module.exports.removeLibro = function(req, res) {
+	Libro.remove({_id : req.params.libro_id}, function(err, libro) {
+		if (err)
+			res.send(err);
+			Libro.find(function(err, libro) {
+				if (err)
+					res.send(err)
+				res.json(libro);
 			});
 		});
 }
