9f27dd899829eff9cf937b4381896eedf81e2a8d
Your Name <ccsjgs1990@gmail.com>
2016-06-13 18:01:23 -0300

    tabla libro.js

--- app/modelo/libro.js
+++ app/modelo/libro.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Libro', {
+	isbn:int,
 	nombre: String,
 	preciolocal:float,
 	autor:String,
 	//portada:??
 	editorial:String,
-	ranking:float
+	ranking:float,
+	genero:String,
+	paginas:int
 });
